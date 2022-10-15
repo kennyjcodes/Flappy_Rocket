e972c376960b534ec0473893969611524c8cce22
Gulraiz <gulraiznoorbari@gmail.com>
2022-10-15 13:50:36 +0500

    🐛 FIX: Camera Follows Player

--- Scripts/Others/CameraFollow.cs
+++ Scripts/Others/CameraFollow.cs
@@ -2,4 +2,15 @@
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField] private Transform _target;
+
+    // Update is called once per frame
+    private void Update()
+    {
+		transform.position = new Vector3(
+            _target.position.x, 
+            _target.position.y, 
+            transform.position.z
+        );
+    }
 }
